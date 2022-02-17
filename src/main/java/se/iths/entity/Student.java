664554008cdb3642de526e06da3b18cd75915f87
Felix Jacobsen <felix.jacobsen@iths.se>
2022-02-17 19:04:25 +0100

    Add student fields and CRUD functionality in StudentService

--- src/main/java/se/iths/entity/Student.java
+++ src/main/java/se/iths/entity/Student.java
@@ -1,12 +1,72 @@
 package se.iths.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
 
 public class Student {
 
+    @Id
     private Long id;
+
+
     private String firstName;
+    @NotEmpty(message = "Cannot be empty")
+    @Size(min = 2, max = 30,message = "Must be minimum two and maximum 30 symbols")
+
+    @NotEmpty(message = "Cannot be empty")
+    @Size(min = 2, max = 30,message = "Must be minimum two and maximum 30 symbols")
     private String lastName;
+
+    @Email(regexp = " ")
+    @NotEmpty(message = "Cannot be empty")
     private String email;
+
+
     private String phoneNumber;
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -1,7 +1,51 @@
 package se.iths.service;
 
+import se.iths.entity.Student;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
 public class StudentService {
 
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public void createStudent(Student student){
+        entityManager.persist(student);
+    }
+
+    public void deleteStudent(Long id){
+        Student deletedStudent = entityManager.find(Student.class,id);
+        try{
+            entityManager.remove(deletedStudent);
+        }catch (NullPointerException e){
+            System.out.println("Student does not exist");
+        }
+    }
+
+    public List<Student> getAllStudents(){
+        TypedQuery<Student> findAll = entityManager.createQuery("SELECT s FROM Student s",Student.class);
+        return findAll.getResultList();
+    }
+
+
+    public void updateStudent(Student newStudent,Long id){
+        Student updateStudent = findById(id);
+        if(updateStudent != null){
+            updateStudent.setFirstName(newStudent.getFirstName());
+            updateStudent.setLastName(newStudent.getLastName());
+            updateStudent.setEmail(newStudent.getEmail());
+            updateStudent.setPhoneNumber(newStudent.getPhoneNumber());
+        }
+        entityManager.persist(newStudent);
+    }
+
+    public Student findById(Long id){
+        return entityManager.find(Student.class,id);
+    }
 
 }
